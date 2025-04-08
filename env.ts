4d865cbf3f3e727c59726d767d9c8354d0882040
Dariusz Filipiak <darekf77@gmail.com>
2025-04-08 15:35:59 +0200

    release: v18.0.22

---
+++ env.ts
@@ -0,0 +1,9 @@
+import type { EnvOptions } from 'tnp/src';
+
+const env: Partial<EnvOptions> = {
+  website: {
+    domain: 'typescript-class-helpers.example.domain.com',
+    title: 'Typescript Class Helpers',
+  },
+};
+export default env;
