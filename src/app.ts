25e94772ece94bf510b61d986f7cd0bbba911aae
Dariusz Filipiak <darekf77@gmail.com>
2025-08-23 13:41:09 +0200

    chore: api update

--- src/app.ts
+++ src/app.ts
@@ -1,34 +1,204 @@
-//#region @notForNpm
-//#region @browser
-import { NgModule } from '@angular/core';
-import { Component, OnInit } from '@angular/core';
+//#region imports
+import * as os from 'os'; // @backend
 
-@Component({
-  selector: 'app-typescript-class-helpers',
-  template: 'hello from typescript-class-helpers'
-})
-export class TypescriptClassHelpersComponent implements OnInit {
-  constructor() { }
+import { CommonModule } from '@angular/common'; // @browser
+import { NgModule, inject, Injectable } from '@angular/core'; // @browser
+import { Component, OnInit } from '@angular/core'; // @browser
+import { VERSION } from '@angular/core'; // @browser
+import Aura from '@primeng/themes/aura'; // @browser
+import { MaterialCssVarsModule } from 'angular-material-css-vars'; // @browser
+import { providePrimeNG } from 'primeng/config'; // @browser
+import { Observable, map } from 'rxjs';
+import { Taon, BaseContext, TAON_CONTEXT } from 'taon/src';
+import { UtilsOs } from 'tnp-core/src';
 
-  ngOnInit() { }
-}
-
-@NgModule({
-  imports: [],
-  exports: [TypescriptClassHelpersComponent],
-  declarations: [TypescriptClassHelpersComponent],
-  providers: [],
-})
-export class TypescriptClassHelpersModule { }
+import { HOST_CONFIG } from './app.hosts';
 //#endregion
 
-//#region @backend
-async function start(port: number)  {
+console.log('hello world');
+console.log('Your backend host ' + HOST_CONFIG['MainContext'].host);
+console.log('Your frontend host ' + HOST_CONFIG['MainContext'].frontendHost);
 
+//#region typescript-class-helpers component
+//#region @browser
+@Component({
+  selector: 'app-typescript-class-helpers',
+  standalone: false,
+  template: `hello from typescript-class-helpers<br />
+    Angular version: {{ angularVersion }}<br />
+    <br />
+    users from backend
+    <ul>
+      <li *ngFor="let user of users$ | async">{{ user | json }}</li>
+    </ul>
+    hello world from backend: <strong>{{ hello$ | async }}</strong> `,
+  styles: [
+    `
+      body {
+        margin: 0px !important;
+      }
+    `,
+  ],
+})
+export class TypescriptClassHelpersComponent {
+  angularVersion =
+    VERSION.full +
+    ` mode: ${UtilsOs.isRunningInWebSQL() ? ' (websql)' : '(normal)'}`;
+  userApiService = inject(UserApiService);
+  readonly users$: Observable<User[]> = this.userApiService.getAll();
+  readonly hello$ = this.userApiService.userController
+    .helloWorld()
+    .request().observable.pipe(map(r => r.body.text));
+}
+//#endregion
+//#endregion
+
+//#region  typescript-class-helpers api service
+//#region @browser
+@Injectable({
+  providedIn: 'root',
+})
+export class UserApiService extends Taon.Base.AngularService {
+  userController = this.injectController(UserController);
+  getAll(): Observable<User[]> {
+    return this.userController
+      .getAll()
+      .request().observable.pipe(map(r => r.body.json));
+  }
+}
+//#endregion
+//#endregion
+
+//#region  typescript-class-helpers module
+//#region @browser
+@NgModule({
+  providers: [
+    {
+      provide: TAON_CONTEXT,
+      useFactory: () => MainContext,
+    },
+    providePrimeNG({
+      // inited ng prime - remove if not needed
+      theme: {
+        preset: Aura,
+      },
+    }),
+  ],
+  exports: [TypescriptClassHelpersComponent],
+  imports: [
+    CommonModule,
+    MaterialCssVarsModule.forRoot({
+      // inited angular material - remove if not needed
+      primary: '#4758b8',
+      accent: '#fedfdd',
+    }),
+  ],
+  declarations: [TypescriptClassHelpersComponent],
+})
+export class TypescriptClassHelpersModule {}
+//#endregion
+//#endregion
+
+//#region  typescript-class-helpers entity
+@Taon.Entity({ className: 'User' })
+class User extends Taon.Base.AbstractEntity {
+  //#region @websql
+  @Taon.Orm.Column.String()
+  //#endregion
+  name?: string;
+}
+//#endregion
+
+//#region  typescript-class-helpers controller
+@Taon.Controller({ className: 'UserController' })
+class UserController extends Taon.Base.CrudController<User> {
+  // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+  entityClassResolveFn = () => User;
+
+  @Taon.Http.GET()
+  helloWorld(): Taon.Response<string> {
+    //#region @websqlFunc
+    return async (req, res) => 'hello world';
+    //#endregion
+  }
+
+  @Taon.Http.GET()
+  getOsPlatform(): Taon.Response<string> {
+    //#region @websqlFunc
+    return async (req, res) => {
+      //#region @backend
+      return os.platform(); // for normal nodejs backend return real value
+      //#endregion
+      return 'no-platform-inside-browser-and-websql-mode';
+    };
+    //#endregion
+  }
+}
+//#endregion
+
+//#region  typescript-class-helpers migration
+//#region @websql
+@Taon.Migration({
+  className: 'UserMigration',
+})
+class UserMigration extends Taon.Base.Migration {
+  userController = this.injectRepo(User);
+  async up(): Promise<any> {
+    const superAdmin = new User();
+    superAdmin.name = 'super-admin';
+    await this.userController.save(superAdmin);
+  }
+}
+//#endregion
+//#endregion
+
+//#region  typescript-class-helpers context
+var MainContext = Taon.createContext(() => ({
+  ...HOST_CONFIG['MainContext'],
+  contexts: { BaseContext },
+  //#region @websql
+  /**
+   * This is dummy migration - you DO NOT NEED need this migrations object
+   * if you are using HOST_CONFIG['MainContext'] that contains 'migrations' object.
+   * DELETE THIS 'migrations' object if you use taon CLI that generates
+   * migrations automatically inside /src/migrations folder.
+   */
+  migrations: {
+    UserMigration,
+  },
+  //#endregion
+  controllers: {
+    UserController,
+  },
+  entities: {
+    User,
+  },
+  database: true,
+  // disabledRealtime: true,
+}));
+//#endregion
+
+async function start(startParams?:Taon.StartParams): Promise<void> {
+  await MainContext.initialize();
+
+   //#region @backend
+   if (startParams.onlyMigrationRun || startParams.onlyMigrationRevertToTimestamp) {
+    process.exit(0);
+  }
+  //#endregion
+
+  //#region @backend
+  console.log(`Hello in NodeJs backend! os=${os.platform()}`);
+  //#endregion
+
+  if (UtilsOs.isBrowser) {
+    const users = (
+      await MainContext.getClassInstance(UserController).getAll().request()
+    ).body?.json;
+    console.log({
+      'users from backend': users,
+    });
+  }
 }
 
 export default start;
-
-//#endregion
-
-//#endregion
