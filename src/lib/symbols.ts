569fbdaa3dafcd3b1e17075a49533c39de89125a
Dariusz Filipiak <darekf77@gmail.com>
2025-10-26 05:51:41 +0100

    chore: classname static property from core models

--- src/lib/symbols.ts
+++ src/lib/symbols.ts
@@ -1,7 +1,9 @@
+import { CoreModels } from "tnp-core/src"
+
 export const SYMBOL = {
   MODELS_MAPPING: Symbol(),
   DEFAULT_MODEL: Symbol(),
   STORAGE: 'classesstorage',
   CLASSES: 'classes',
-  ClassNameStaticProperty : '$$className$$'
+  ClassNameStaticProperty : CoreModels.ClassNameStaticProperty,
 }
