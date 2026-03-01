f46dc3a2eaa5c1b0ddd538119107a03d4a4367f8
Dariusz Filipiak <darekf77@gmail.com>
2026-03-01 02:29:07 +0100

    refactor: - simple stor for classes - static propery for setting name

--- src/lib/registerd-db.ts
+++
@@ -1,5 +0,0 @@
-import { Models } from './models';
-export const registerd = {
-  configs: [] as Models.ClassConfig[],
-  classes: [] as Function[],
-}
--- src/lib/symbols.ts
+++
@@ -1,9 +0,0 @@
-import { CoreModels } from "tnp-core/src"
-
-export const SYMBOL = {
-  MODELS_MAPPING: Symbol(),
-  DEFAULT_MODEL: Symbol(),
-  STORAGE: 'classesstorage',
-  CLASSES: 'classes',
-  ClassNameStaticProperty : CoreModels.ClassNameStaticProperty,
-}
